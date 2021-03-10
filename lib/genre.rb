62513bf9cdb7eeb3d569e649cb08c006ba8f4489
speraza545 <s.peraza545@gmail.com>
2021-03-10 10:14:00 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,33 @@
 
+class Genre
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(genre)
+        @name = genre
+        self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def save
+        @@all << self
+    end
+
+    def songs
+        Song.all.select{|song| song.genre == self}
+    end
+
+    def artists
+        artists_uniq = []
+        self.songs.map do |song|
+            if song.genre == self
+                artists_uniq << song.artist
+            end
+        end
+        artists_uniq.uniq
+    end
+end
