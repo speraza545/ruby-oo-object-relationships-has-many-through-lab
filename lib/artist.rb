f421cba476371cf3a493b3a1c624883e5d63996f
speraza545 <s.peraza545@gmail.com>
2021-03-10 10:13:56 -0500

    commit tree 4f8e115f4f915c62ef12175c52bc2a0a6128a152

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,32 @@
+class Artist
+    attr_accessor :name, :genre
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_song(name, genre)
+        new_song = Song.new(name, self, genre)
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
+    end
+
+    def genres
+        self.songs.map do |song|
+            song.genre
+        end
+    end
+end
